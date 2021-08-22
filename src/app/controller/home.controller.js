40ad63da68d434752eb673f5739b6c44bea2ff3d
namvt002 <viettrung0601@gmail.com>
2021-08-22 07:33:05 +0700

    new

--- src/app/controller/home.controller.js
+++ src/app/controller/home.controller.js
@@ -1,6 +1,6 @@
 class HomeController {
     index(req, res) {
-        res.render('pages/home', {css: "/css/home.css"});
+        res.render('pages/home', {css: "/css/home.css", title: 'Shopping'});
     }
 }
 
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -18,14 +18,42 @@ var SmallImg = document.getElementsByClassName("small-img");
         ProductImg.src = SmallImg[0].src;
     }
     SmallImg[1].onclick = function(){
-        ProductImg.src = SmallImg[1].src;
+        ProductImg.src =  SmallImg[1].src;
     }
     SmallImg[2].onclick = function(){
-        ProductImg.src = SmallImg[2].src;
+        ProductImg.src =SmallImg[2].src;
     }
     SmallImg[3].onclick = function(){
         ProductImg.src = SmallImg[3].src;
     }
 
 
-// ---account---
+// đổi màu cho cái chi tiêt sản phẩm
+
+
+const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+let prevColor = "blue";
+let animationEnd = true;
+function changeSize(){
+    sizes.forEach(size => size.classList.remove('active'));
+    this.classList.add('active');
+}
+function changeColor(){
+    if(!animationEnd) return;
+    let primary = this.getAttribute('primary');
+    let color = this.getAttribute('color');
+    //if(color == prevColor) return;
+   
+    colors.forEach(c => c.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+
+}
+sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(c => c.addEventListener('click', changeColor));
+
+
+// --sidershows -----
+
